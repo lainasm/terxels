4266bb63d70537a86fb33eed8e91380fa59f44e4
haedqu <Hampus.Edquist@protonmail.com>
2024-10-08 15:43:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Terxels
+
+## First impressions
+
+A super easy-to-use header only library to freely output pixels in an ANSI terminal. It's the 21st centuary which means..... EACH PIXEL COMES IN 16777216 DIFFERENT COLORS!!! No more horrible, downright awful terminal designs where calling them antiquated would be an insult to antique programs.
+
+## The problem
+
+If you've ever tried to create a highly graphical program in the terminal before, you've probably noticed something, it looks terrible. The reason for that is there is no such thing as a pixel when outputting to a terminal. You instead usually come up with ugly hacks like using "#" or "@" to indicate a full entry in a screen buffer. If you get fed-up of looking at that for a few hours, you might come up with the idea of using a fullblock ascii character to represent it instead. This is significantly better but it's still looks like garbage, which leads us to the second reason terminal graphics look ugly: characters in most fonts are super lanky. Everything you draw using them will look very off and super long.
+
+They key insight to make here is to instead let every fullblock character represent two pixels, not one! This leads to extremely good visual results and manages to solve every single problem you had with terminal graphics before, with one catch of course, it is very hard to work with. To have it function properly you need to tediously switch between drawing different ascii codes, along with switching which ansi codes are used for the foreground and backgrounds, but fear not! This library solves all of this and abstracts it behind a super elegant interface that is dirt simple to not only understand, but to use as well.
+
+## But why?
+
+i think the pretty colors are pretty, ok :3
+
+## Okay, how do I use it then?
+
+The easiest way to learn is to simply look at the examples for a basic understanding, after that looking directly in the source code should hopefully not be too confusing. Here is a simple minimal example that shows how you could use terxel to visualize something programmatically:
+
+```cpp
+#include "terxels.h"
+
+int main()
+{
+    terxel::Terxture terxture(32, 32);
+    for (unsigned char y = 0; y < 32; y++)
+    {
+        for (unsigned char x = 0; x < 32; x++)
+        {
+            terxture.SetPixel(x, y, {(unsigned char)(x * 4), (unsigned char)(y * 4), (unsigned char)((x + y) * 4)});
+        }
+    }
+    terxture.Draw();
+}
+```
+Of course, you can always remove the terxel namespace with using namespace if you wish to do so.
---
+++ terxels.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+
+namespace terxel
+{
+    class Color
+    {
+        public:
+            unsigned char r = 0;
+            unsigned char g = 0;
+            unsigned char b = 0;
+
+            bool enabled = false;
+    };
+
+    class Terxel
+    {
+        public:
+            Terxel() = default;
+
+            Terxel(Color first, Color second)
+            {
+                top = first;
+                bottom = second;
+            }
+
+            void Draw()
+            {
+                if (!bottom.enabled && !top.enabled)
+                {
+                    printf(" ");
+                }
+
+                else if (!bottom.enabled)
+                {
+                    printf("\033[48;5;0;38;2;%d;%d;%dm▀", top.r, top.g, top.b);
+                }
+
+                else if (!top.enabled)
+                {
+                    printf("\033[38;2;%d;%d;%d;48;5;0m▄", bottom.r, bottom.g, bottom.b);
+                }
+
+                else
+                {
+                    printf("\033[38;2;%d;%d;%d;48;2;%d;%d;%dm▄", bottom.r, bottom.g, bottom.b, top.r, top.g, top.b);
+                }
+            }
+
+        public:
+            Color top;
+            Color bottom;
+    };
+
+    class Terxture
+    {
+        public:
+            Terxture(int width, int height, int pixelScale = 1)
+                :
+                    width(width * pixelScale),
+                    height((int)((float)height * pixelScale / 2.0f + 0.5f)),
+                    terxels(this->width * this->height),
+                    pixelScale(pixelScale)
+        {
+        }
+
+            void SetPixel(int x, int y, Color color)
+            {
+                x *= pixelScale;
+                y *= pixelScale;
+
+                for (int yOffset = 0; yOffset < pixelScale; yOffset++)
+                {
+                    for (int xOffset = 0; xOffset < pixelScale; xOffset++)
+                    {
+                        int pixelX = x + xOffset;
+                        int pixelY = y + yOffset;
+
+                        if (pixelY % 2 == 0)
+                        {
+                            terxels[pixelY / 2 * width + pixelX].top = color;
+                            terxels[pixelY / 2 * width + pixelX].top.enabled = true;
+                        }
+
+                        else
+                        {
+                            terxels[pixelY / 2 * width + pixelX].bottom = color;
+                            terxels[pixelY / 2 * width + pixelX].bottom.enabled = true;
+                        }
+                    }
+                }
+
+            }
+
+            void Draw()
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        terxels[y * width + x].Draw();
+                    }
+
+                    printf("\033[39;m");
+                    printf("\033[49;m");
+                    printf("\n");
+                }
+
+                printf("\033[39;m");
+                printf("\033[49;m");
+            }
+
+        private:
+            const int width;
+            const int height;
+            std::vector<Terxel> terxels;
+
+            const int pixelScale;
+    };
+}
